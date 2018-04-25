51547292533c65b8c8c2629ef3111e0b016acb14
dylannichols <nichodo@g.toiohomai.ac.nz>
2018-04-25 15:41:51 +1200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace assignment1_part4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
